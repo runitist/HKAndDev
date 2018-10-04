7f992d215a35a69f30f78e3a430b3436d02296e1
DESKTOP-JE7JPJ3\Etha <runitist@gmail.com>
2018-10-04 19:42:17 +0900

    Main뷰 완성 및 스트링, 스타일 리펙토링

--- app/src/main/java/com/hkanddev/anddev/MainActivity.java
+++ app/src/main/java/com/hkanddev/anddev/MainActivity.java
@@ -1,13 +1,31 @@
 package com.hkanddev.anddev;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button calc_btn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        setTitle(R.string.main_title);
+    }
+
+    public void main_clk(View view) {
+        calc_btn = (Button)view;
+        if(calc_btn.getId()==(R.id.main_btn1)){
+
+        }else if(calc_btn.getId()==(R.id.main_btn2)){
+
+        }
     }
 }
