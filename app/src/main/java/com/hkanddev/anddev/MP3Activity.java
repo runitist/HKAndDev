59934fd03afae77ca5ecc72ab5ad4fc03e381d58
DESKTOP-JE7JPJ3\Etha <runitist@gmail.com>
2018-10-04 21:30:12 +0900

    MP3 완성

--- app/src/main/java/com/hkanddev/anddev/MP3Activity.java
+++ app/src/main/java/com/hkanddev/anddev/MP3Activity.java
@@ -1,9 +1,17 @@
 package com.hkanddev.anddev;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MP3Activity extends AppCompatActivity {
+    private Button btn = null;//버튼의 객체
+    private int id = 0;//각 버튼의 id값
+    private int stat = 0;//음악의 상태 시작 : 1, 정지 ==0, 일시정지 ==-1
+    private MediaPlayer mp = null;//음악 플레이어 객체
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,76 @@ public class MP3Activity extends AppCompatActivity {
 
     private void _init() {
         setTitle(R.string.mp3_title);
+    }
+
+    public void mp3clk(View view) {
+        btn = (Button) view;
+        id = btn.getId();
+        if (id == R.id.music1) {
+            stopTheMusic();
+            mp = MediaPlayer.create(this, R.raw.believer);
+            mp.start();
+            stat = 1;
+        } else if (id == R.id.music2) {
+            stopTheMusic();
+            mp = MediaPlayer.create(this, R.raw.dragonblade);
+            mp.start();
+            stat = 1;
+        } else if (id == R.id.music3) {
+            stopTheMusic();
+            mp = MediaPlayer.create(this, R.raw.galwaygirl);
+            mp.start();
+            stat = 1;
+        } else if (id == R.id.music4) {
+            stopTheMusic();
+            mp = MediaPlayer.create(this, R.raw.natural);
+            mp.start();
+            stat = 1;
+        } else if (id == R.id.music5) {
+            stopTheMusic();
+            mp = MediaPlayer.create(this, R.raw.sevenyears);
+            mp.start();
+            stat = 1;
+        } else if (id == R.id.music6) {
+            stopTheMusic();
+            mp = MediaPlayer.create(this, R.raw.shapeofyou);
+            mp.start();
+            stat = 1;
+        } else if (id == R.id.pause) {
+            if (mp != null) {
+                if (stat == 1) {
+                    mp.pause();
+                    stat = -1;
+                } else if (stat == -1) {
+                    mp.start();
+                    stat = 1;
+                }
+            } else {
+                Toast.makeText(this, "재생할 노래를 선택해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.stop) {
+            stopTheMusic();
+        }
+        pauseTextUpdate();
+    }
+
+    private void stopTheMusic() {
+        //음악 객체를 비우는 메서드
+        if (mp != null) {
+            mp.stop();
+            mp = null;
+            stat = 0;
+        }
+    }
+
+    private void pauseTextUpdate() {
+        //일시정지 버튼의 텍스트를 변경하는 메서드
+        if (stat == 1) {
+            Button btn = findViewById(R.id.pause);
+            btn.setText(getText(R.string.pauseTextUpdate1));
+        } else if (stat != 1) {
+            Button btn = findViewById(R.id.pause);
+            btn.setText(getText(R.string.pauseTextUpdate2));
+        }
     }
 }
--- app/src/main/java/com/hkanddev/anddev/MainActivity.java
+++ app/src/main/java/com/hkanddev/anddev/MainActivity.java
@@ -5,28 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button calc_btn = null;
+    Button btn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //이 액티비티가 실행되면 최초로 실행되는 메서드.
+        super.onCreate(savedInstanceState);//조상타입을 실행시킴.
+        setContentView(R.layout.activity_main);//이 액티비티와 이어지는 뷰.
         _init();
     }
 
     private void _init() {
-        setTitle(R.string.main_title);
+        //액티비티가 실행될 때 같이 실행되는 메서드. 초기화로 사용.
+        setTitle(R.string.main_title);//액션바 타이틀을 바꿈.
     }
 
     public void main_clk(View view) {
-        calc_btn = (Button) view;
-        if (calc_btn.getId() == (R.id.main_btn1)) {
+        btn = (Button) view;//매개변수를 받아와 객체화후 버튼 변수에 집어넣음.
+        if (btn.getId() == (R.id.main_btn1)) {
             Intent intent = new Intent(this, CalculatorActivity.class);
+            //이 액티비티에서 다른 클래스 액티비티로 이동.
+            Toast.makeText(this, "계산기로 이동합니다.", Toast.LENGTH_SHORT).show();
             startActivity(intent);
-        } else if (calc_btn.getId() == (R.id.main_btn2)) {
+            //위에서 설정한 인텐트를 실행시킴.
+        } else if (btn.getId() == (R.id.main_btn2)) {
             Intent intent = new Intent(this, MP3Activity.class);
+            Toast.makeText(this, "MP3로 이동합니다.", Toast.LENGTH_SHORT).show();
             startActivity(intent);
         }
     }
